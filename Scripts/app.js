2a31b39b37cdc8c6ed15340881ee729cf0706293
dalbay <fadalbay@gmail.com>
2019-12-16 11:22:30 -0500

    website comp update

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,42 +1,50 @@
-
-
 /*Login button - Displays Login Modal*/
-var btnLogin = document.getElementById("btnLogin").addEventListener('click',function(){modalLogin.style.display='block'});
+var btnLogin = document
+  .getElementById("btnLogin")
+  .addEventListener("click", function() {
+    modalLogin.style.display = "block";
+  });
 var modalLogin = document.getElementById("loginModal");
 
 /*SignUp button - Displays Signup Modal */
 var modalSignup = document.getElementById("signupModal");
-var btnSignup = document.getElementById("btnSignup").addEventListener('click',function(){modalSignup.style.display='block'});
+var btnSignup = document
+  .getElementById("btnSignup")
+  .addEventListener("click", function() {
+    modalSignup.style.display = "block";
+  });
 
 /*Hide the Modal*/
-document.getElementById('loginClose').addEventListener('click',()=>{modalLogin.style.display='none'});
-document.getElementById('signupClose').addEventListener('click',()=>{modalSignup.style.display='none'});
-
-
+document.getElementById("loginClose").addEventListener("click", () => {
+  modalLogin.style.display = "none";
+});
+document.getElementById("signupClose").addEventListener("click", () => {
+  modalSignup.style.display = "none";
+});
 
 // Close app button
-function redirectToIndex(){
-    window.location = "index.html";
+function redirectToIndex() {
+  window.location = "index.html";
 }
 
-
 function myFunction() {
-    var x = document.title;
-    switch(x){
+  var x = document.title;
+  switch (x) {
+    case "New Time Sheet":
+      document.getElementById("newTimeSheet").style.backgroundColor = "#24aae1";
+      break;
+    case "Current Time Sheet":
+      document.getElementById("currentTimeSheet").style.backgroundColor =
+        "#24aae1";
+      break;
 
-                case "New Time Sheet":
-            document.getElementById('newTimeSheet').style.backgroundColor= '#3a8668';
-                break;
-        case "Current Time Sheet":
-            document.getElementById('currentTimeSheet').style.backgroundColor= '#3a8668';
-              break;
-
-        case 'Personal Info':
-                document.getElementById('personalInfo').style.backgroundColor= '#3a8668';
-                break;
-        case 'Previous Time Sheet' :
-                document.getElementById('previousTimeSheet').style.backgroundColor= '#3a8668';
-    }
+    case "Personal Info":
+      document.getElementById("personalInfo").style.backgroundColor = "#24aae1";
+      break;
+    case "Previous Time Sheet":
+      document.getElementById("previousTimeSheet").style.backgroundColor =
+        "#24aae1";
   }
+}
 
-  document.querySelector("body").addEventListener('load',myFunction);
+document.querySelector("body").addEventListener("load", myFunction);
