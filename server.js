a2b9fb55ebc6dbd81c7eca9bb62daac7fd20a051
dalbay <fadalbay@gmail.com>
2020-05-13 19:53:39 -0400

    npm installed

--- server.js
+++ server.js
@@ -3,21 +3,24 @@ const http = require("http");
 // create server
 const server = http.createServer((req, res) => {
   const pathName = req.url;
-
-  if (pathName === "/" || pathName === "/overview") {
-    res.end("This is the OVERVIEW");
+  // Home page
+  if (pathName === "/" || pathName === "/index") {
+    // Product page
+    res.end("This is the Home Page");
   } else if (pathName === "/product") {
     res.end("This is the PRODUCT");
+    // API
   } else if (pathName === "/api") {
     /*
-        Building a (Very) Simple API
-    First add another route(/api) to our project, and a simple placeholder for the response.
-    Next,
-        - Read data from the data.json file, then
-        - parse JSON into a JavaScript object, and then
-        - send back the result to the client.
-    */
+            Building a (Very) Simple API
+        First add another route(/api) to our project, and a simple placeholder for the response.
+        Next,
+            - Read data from the data.json file, then
+            - parse JSON into a JavaScript object, and then
+            - send back the result to the client.
+        */
     res.end("This is an API");
+    // Not  Found
   } else {
     res.writeHead(404, {
       "Content-type": "text/html",
